cd29ea883de49cb92587fa1a73957a24547873c1
Wyatt  pefley <wyattpefley@Wyatts-MacBook-Pro.local>
2017-11-12 19:35:58 -0800

    finished second question, making new branch.

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(){ //eslint-disable-line
-var sum = 4 + 7;
-var arrayOne = [sum, 'The sum of 4 and 7 is 11.'];
-console.log(arrayOne);
+function sum(a,b){ //eslint-disable-line
+var sum = a + b;
+var arrayOne = [sum, 'The sum of '+a+' and '+b+' is '+sum+'.'];
 return arrayOne;
 }
 
@@ -26,11 +25,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
-
+var product = a * b;
+var productArray = [product, 'The product of '+a+' and '+b+' is '+product+'.'];
+return productArray;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
